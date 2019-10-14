37acc227472d86fa99025e5f984e709b983c034c
Griffin Noe <steelersfan437@gmail.com>
2019-10-14 12:02:13 -0400

    circle constructor
    Added a constructor to the circle class.

--- Circle.java
+++ Circle.java
@@ -1,3 +1,18 @@
 public class Circle extends Shape{
+    private Int radius;
     
+    public Circle(double radius, int xCoordinate, int yCoordinate, String color, boolean filled){
+        super(xCoordinate,yCoordinate,color,filled);
+        this.radius = radius;
+        shapeType = "Circle";
+    }
+
+    public Int getRadius(){
+        return radius;
+    }
+
+    public void setRadius(Int radius){
+        this.radius = radius;
+    }
+
 }
