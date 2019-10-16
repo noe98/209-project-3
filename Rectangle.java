1988c0e391bbf74b12bcd24dc3c1824fcdcbb326
ElyssaMcMaster <56412984+ElyssaMcMaster@users.noreply.github.com>
2019-10-16 16:38:35 -0400

    ints and toString
    changed doubles to ints, edited toString

--- Rectangle.java
+++ Rectangle.java
@@ -1,16 +1,17 @@
 
 
-public class Square extends Shape{
+public class Rectangle extends Shape{
     private int[] xCoords;
     private int[] yCoords;
     private int width;
+    private int height;
 
 
 
     // methods to find initial x coord, y coord, fill, 
     // and color have already been included in the class
     // this one inheirits from
-    public Square(int width, int xCoordinate, int yCoordinate, String color, boolean filled){
+    public Rectangle(int width, int xCoordinate, int yCoordinate, String color, boolean filled){
         super(xCoordinate, yCoordinate, color, filled);
         int[] xCoords = new int[3];
         xCoords[0]= xCoordinate;
@@ -30,10 +31,15 @@ public class Square extends Shape{
         return width;
     }
         
+    public int getHeight(){
+        return height;
+    }
+
     public String toString(){
         String str = super.toString();
-        str += "==Square==";
+        str += "==Rectangle==";
         str += "\nWidth: " + width;
+        str += "\nHeight: " + height;
         str += "\nCoordinates: " + "(" +xCoords[0] + "," + yCoords[0] +")";
         str += ", (" + xCoords[1] + "," +yCoords[1] +")";
         str += ", (" + xCoords[2] + "," +yCoords[2] +")";
@@ -43,10 +49,10 @@ public class Square extends Shape{
         str += "\nColor: " + color;
         str += "\n";
         str += "\nSide 1: " + width;
-        str += "\nSide 2: " + width;
+        str += "\nSide 2: " + height;
         str += "\nSide 3: " + width;
-        str += "\nSide 4: " + width;
-        str += "\nPerimeter: " + 4 * width;
+        str += "\nSide 3: " + height;
+        str += "\nPerimeter: " + 2 * width + 2 * height;
         str += "\n====";
         return str;
     }
