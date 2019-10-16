b6f93e796945354a0a44dcffeb148157a6f659bd
ElyssaMcMaster <56412984+ElyssaMcMaster@users.noreply.github.com>
2019-10-16 17:25:31 -0400

    commit PolygonTester
    a mess but I'm trying; cannot find main class

---
+++ PolygonTester.java
@@ -0,0 +1,7 @@
+
+class PolygonTester{
+    public static void main(String[] args) {
+        Square testsquare = new Square(2, 2, 2, "black", true);
+        System.out.println(testsquare);
+    }
+}
