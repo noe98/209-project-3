a281305c6caf4d1463bb9f58dfd02ee525bb9f34
Griffin Noe <steelersfan437@gmail.com>
2019-10-16 21:34:54 -0400

    Added printline for triangle
    Added printline for triangle in shapetester class

--- ShapeTester.java
+++ ShapeTester.java
@@ -5,6 +5,7 @@ public class ShapeTester{
         Rectangle r = new Rectangle(2,3,1,1,"PINK",true);
         System.out.println(r);
         Triangle t = new Triangle(2,3,1,1,"PURPLE",false);
+        System.out.println(t);
         Circle c = new Circle(3,1,1,"BLUE",true);
         System.out.println(c);
         Oval o = new Oval(3,2,1,1,"GREEN", false);
