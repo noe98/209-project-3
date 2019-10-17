0e8f6f63674fbf267689f28ed4372b87425f6cde
Griffin Noe <steelersfan437@gmail.com>
2019-10-16 21:31:35 -0400

    Shape Tester
    Added instantiation of all shapes.

--- ShapeTester.java
+++ ShapeTester.java
@@ -1,5 +1,13 @@
 public class ShapeTester{
     public static void main(String[] args) {
-        System.out.println("hello");
+        Square s = new Square(2,1,1,"GREEN",false);
+        System.out.println(s);
+        Rectangle r = new Rectangle(2,3,1,1,"PINK",true);
+        System.out.println(r);
+        Triangle t = new Triangle(2,3,1,1,"PURPLE",false);
+        Circle c = new Circle(3,1,1,"BLUE",true);
+        System.out.println(c);
+        Oval o = new Oval(3,2,1,1,"GREEN", false);
+        System.out.println(o);
     }
 }
